91f8a0ef2197ddde4cee9ae6d24e22187d38cb74
ppppminji <pys619@naver.com>
2024-04-02 10:39:21 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+var http=require('http');
+var hostname='127.0.0.1';
+var port="8000";
+
+const server=http.createServer(function(req, res){
+    const path=req.url;
+    const method=req.method;
+
+    if(path==="/products"){
+        if(method==="GET"){
+            res.writeHead(200,{"Content-Type":"application/json"});
+            const products=JSON.stringify([
+                {
+                    name:"apple Mac",
+                    price:399000,
+
+                },
+            ]);
+            res.end(products)
+        }else if(method==="POST"){
+            res.end("생성 되었습니다.")
+        }
+    }
+    res.end("bye")
+})
+
+server.listen(port, hostname);
+console.log('apple-server on')
